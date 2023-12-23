096bf067ae4842399c8f2943fe8ba0d71ab71e44
raylin009 <raylin009@gmail.com>
2023-12-23 14:17:23 -0800

    Tutorial 2 complete

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: false,
+    userDataDir: "./tmp",
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png'});
+  await page.goto('https://www.amazon.com/s?k=office+chair+wheels&crid=191F26OFNMQVU&sprefix=office+chair+wheels%2Caps%2C162&ref=nb_sb_noss_1');
 
-  await browser.close();
+  const productsHandler = await page.$$(' div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item')
+
+  let items = [];
+
+  for (const producthandle of productsHandler){
+    let title = "Null";
+    let price = "Null";
+    let imgURL = "Null";
+
+    try {
+      title = await page.evaluate(el => el.querySelector("h2 > a > span").textContent, producthandle)
+    } catch (error) {}
+    try {
+      imgURL = await page.evaluate(el => el.querySelector(".s-image").getAttribute("src"), producthandle)
+    } catch (error) {}
+    try {
+      price = await page.evaluate(el => el.querySelector(".a-price > .a-offscreen").textContent, producthandle)
+    } catch (error) {}
+
+    if(title !== "Null"){
+      items.push({title, price, imgURL})
+    }
+  }
+
+  console.log(items.length)
 })();
