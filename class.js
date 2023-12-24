ffa565978b9cca308fad33761e7b8f8006689874
raylin009 <raylin009@gmail.com>
2023-12-23 21:57:01 -0800

    successfully scrap all items from all 7 pages

---
+++ class.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer');
+const domain = 'https://www.amazon.com/s?k=office+chair+wheels&page=6&crid=191F26OFNMQVU&qid=1703391448&sprefix=office+chair+wheels%2Caps%2C162&ref=sr_pg_6'
+const scraper = async (domain) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(domain, { waitUntil: "load"})
+
+  // const next_page_button_status = await page.evaluate(ele => ele.classList.contains('s-pagination-disabled'), next_page_button)
+  let next_page_button_status = false
+  
+  while (next_page_button_status === false){
+    //get data from page
+    
+    await new Promise((resolve)=>setTimeout(resolve, 1000))
+    console.log("wait complete")
+    //go to next page
+    const next_page_button = await page.$('.s-pagination-next');
+    await next_page_button.click()
+    //update next_page_button_status
+    next_page_button_status = await page.evaluate(ele => ele.classList.contains('s-pagination-disabled'), next_page_button)
+
+  } 
+
+
+  // console.log(next_page_button_status)
+
+
+
+  await browser.close();
+
+}
+
+scraper(domain)
+
+
+
+// page 1 next button attribute
+// s-pagination-item s-pagination-next s-pagination-button s-pagination-separator
+
+// pade 7 next button attribute
+// s-pagination-item s-pagination-next s-pagination-disabled 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -7,29 +8,43 @@ import puppeteer from 'puppeteer';
     userDataDir: "./tmp",
   });
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.com/s?k=office+chair+wheels&crid=191F26OFNMQVU&sprefix=office+chair+wheels%2Caps%2C162&ref=nb_sb_noss_1');
-
-  const productsHandler = await page.$$(' div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item')
+  await page.goto('https://www.amazon.com/s?k=office+chair+wheels&crid=191F26OFNMQVU&qid=1703376409&sprefix=office+chair+wheels%2Caps%2C162&ref=sr_pg_1');
 
   let items = [];
+  let isBtnDisabled = false;
+  
+  while(!isBtnDisabled){
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    const productsHandler = await page.$$(' div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item')
+    for (const producthandle of productsHandler){
+      let title = "Null";
+      let price = "Null";
+      let imgURL = "Null";
 
-  for (const producthandle of productsHandler){
-    let title = "Null";
-    let price = "Null";
-    let imgURL = "Null";
+      try {
+        title = await page.evaluate(el => el.querySelector("h2 > a > span").textContent, producthandle)
+      } catch (error) {}
+      try {
+        imgURL = await page.evaluate(el => el.querySelector(".s-image").getAttribute("src"), producthandle)
+      } catch (error) {}
+      try {
+        price = await page.evaluate(el => el.querySelector(".a-price > .a-offscreen").textContent, producthandle)
+      } catch (error) {}
 
-    try {
-      title = await page.evaluate(el => el.querySelector("h2 > a > span").textContent, producthandle)
-    } catch (error) {}
-    try {
-      imgURL = await page.evaluate(el => el.querySelector(".s-image").getAttribute("src"), producthandle)
-    } catch (error) {}
-    try {
-      price = await page.evaluate(el => el.querySelector(".a-price > .a-offscreen").textContent, producthandle)
-    } catch (error) {}
+      if(title !== "Null"){
+        items.push({title, price, imgURL})
+      }
+    }
 
-    if(title !== "Null"){
-      items.push({title, price, imgURL})
+    await page.waitForSelector(".s-pagination-next", {visible: true})
+    const is_disabled = await page.$('.s-pagination-next.s-pagination-disabled') !== null;
+    isBtnDisabled = is_disabled
+    // console.table({is_disabled, itemsLenght: items.length})
+    if(!is_disabled){
+      await Promise.all([
+        page.click("a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator"),
+        page.waitForNavigation({waitUntil: "networkidle2"})
+      ])
     }
   }
 
