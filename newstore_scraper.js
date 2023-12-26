b9ee8fb8268f460eff0faf4b36f587940ca60118
raylin009 <raylin009@gmail.com>
2023-12-25 17:39:43 -0800

    can get order id, time and link

--- newstore_scraper.js
+++ newstore_scraper.js
@@ -27,8 +27,63 @@ const handleLogIn = async (page) => {
   return 
 }
 
+const setQuaryParam = async(page, store_location, date_range) => {
+  const loading_indicator = 'div.rt-noData';
+  let paramURL = "https://manager.vince.p.newstore.net/sales/orders?page=0&pageSize=100&dir=asc&sortBy=placedAt"
+  const handleStoreLocationQuery = (store_location) => {
+    return `&filters%5BplacementLocation.label%5D%5B%5D=${store_location}`
+  }
+  const handleDateRangeQuery = ({st_date, end_date}) => {
+    return `&filters%5BplacedAt%5D%5Bmin%5D=${st_date}&filters%5BplacedAt%5D%5Bmax%5D=${end_date}`
+  }
+  paramURL = paramURL.concat(handleStoreLocationQuery(store_location), handleDateRangeQuery(date_range))
+  await page.goto(paramURL, {waitUntil: "networkidle0"})
+  await page.waitForSelector(loading_indicator, { hidden: true });
+  return
+}
+
+const setDisplayParam = async(page)=>{
+  const displayParam = {
+    "Associate": true,
+    "Date_time": true,
+    "Channel_type": false,
+    "Channel" : false,
+    "Fulfillment": false,
+    "Demand_location": false,
+    "Fulfillment_location": false,
+    "Carrier": false,
+    "Shipment_method": false,
+    "Total": true,
+    "Discount": false,
+    "Status": false,
+  }
+
+}
+
+const getOrderIdsOnPage = async(page) => {
+  const ordersHandler = await page.$$("div.rt-table > div.rt-tbody > div.rt-tr-group")
+  for( order of ordersHandler){
+    try {
+      const order_id = await page.evaluate( el => el.querySelector("div.rt-tr > div.rt-td > a").textContent, order)
+      const order_href = await page.evaluate( el => el.querySelector("div.rt-tr > div.rt-td > a").href, order)
+      console.table({order_id, order_href})
+    } catch (e) {
+      console.log(e)
+    }
+    try {
+      const date_time = await page.evaluate( el => el.querySelector("span").textContent, order)
+    } catch (e) {
+      console.log(e)
+    }
+    
+  }
+}
+
+
 const newstore_scraper = async() => {
 //goto https://manager.vince.p.newstore.net/
+// const loading_indicator = 'div.rt-noData';
+
 const browser = await puppeteer.launch({
   headless: false,
   defaultViewport: false,
@@ -41,10 +96,22 @@ if(page.url()== new_store_url) {
   await handleLogIn(page, CRED);
 }
 
+//set quary param and sorting order
+await setQuaryParam(page, "5926-SFPREMIUMOUTLETS", {st_date: "2023-12-23", end_date:"2023-12-24"})
+
+//set display parameter
+// await setDisplayParam(page)
+
+//get data page by page
+await getOrderIdsOnPage(page);
+
 //change display rows to 100
-await page.select("span.-pageSizeOptions > select", "100")
-await page.waitForSelector('div.rt-noData', { hidden: true });
-await page.screenshot({path: "screenshot.png"})
+// await page.select("span.-pageSizeOptions > select", "100")
+// await page.waitForSelector(loading_indicator, { hidden: true });
+// await page.screenshot({path: "screenshot.png"})
+// console.log(page)
+
+
 
 
 
